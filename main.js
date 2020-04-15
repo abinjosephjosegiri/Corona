d2f3282ad071cbfd37066db76f78acf6fdee6a57
Abin Joseph <abinjosephjosegiri@gmail.com>
2020-04-15 23:55:58 +0530

    bak to nor

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
-
 signUpButton.addEventListener('click', () => 
 container.classList.add('right-panel-active'));
 signInButton.addEventListener('click', () => 

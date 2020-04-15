bed0a88df4b8a7896ae80746e37fff6adf38bee2
Abin Joseph <abinjosephjosegiri@gmail.com>
2020-04-16 03:13:28 +0530

    sign up ok

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+      console.log('user logged in: ', user);
+    } else {
+      console.log('user logged out');
+    }
+  })
+  
 //signup
 const signupForm = document.querySelector('#SignUpForm');
 signupForm.addEventListener('submit', (e) => {
@@ -24,4 +33,23 @@ signupForm.addEventListener('submit', (e) => {
   location.reload();
   auth.signOut()
   
+});
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+
 });
